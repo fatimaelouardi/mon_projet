a683f1097547c233d85e865a4c279d3ff81ae8fc
fatima <fatima@KYURY.>
2024-11-29 08:21:31 +0100

    tests runs

--- code/server/src/core/server.ts
+++ code/server/src/core/server.ts
@@ -1,20 +1,17 @@
 import express, { type Router, type Express, Request, Response } from "express";
 import http from "node:http";
-import ProduitRouter from "../router/produit_router.js";  // Le routeur des produits
-import SecurityRouter from "../router/security_router.js";  // Routeur pour gérer la sécurité
-import cors from "cors";  // Middleware CORS pour gérer les accès
+import ProduitRouter from "../router/produit_router.js";  
+import SecurityRouter from "../router/security_router.js";  
+import cors from "cors";  
 import OriginMiddleware from "../middleware/security/origin_middleware.js";
-// import TailleRouter from "../router/taille_router.js";
 
 class Server {
-    private app: Express = express();  // Application Express
+    private app: Express = express();  
     private router: Router = express.Router();  
 
     constructor() {
-        // Middleware pour parser le corps des requêtes en JSON
         this.router.use(express.json());
 
-        // dossier stockant les ressources publiques 
 
         this.router.use(express.static(process.env.ASSETS_DIRECTORY as string));
 
@@ -25,29 +22,21 @@ class Server {
             })
         );
 
-        // Vérifier l'origine des requêtes via OriginMiddleware
         this.router.use(new OriginMiddleware().check);
 
-        // Lier l'application au router principal
         this.app.use(this.router);
 
-        // Lister les routeurs
         this.listRouters();
     }
 
-    // Méthode pour lier les différents routeurs à leurs préfixes
     private listRouters = (): void => {
-        // Routeur pour gérer les produits
         this.router.use("/produit", new ProduitRouter().getRouter());
-        // this.router.use("/taille", new TailleRouter().getRouter());
 
-        // Routeur pour gérer la sécurité (authentification, autorisation)
         this.router.use("/", new SecurityRouter().getRouter());
     };
 
-    // Méthode pour créer et retourner le serveur HTTP
     public createServer = (): http.Server => {
-        return http.createServer(this.app);  // Créer et retourner le serveur HTTP
+        return http.createServer(this.app);  
     };
 }
 
